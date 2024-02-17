c5e8b7f0aa7dd6f35ad970c361951804a0a285be
Pedro Santos <phpdro@gmail.com>
2024-02-17 19:10:35 -0300

    wip: testing operator

--- src/response_message.rs
+++ src/response_message.rs
@@ -1,7 +1,5 @@
 use std::fmt::Display;
 
-
-
 pub struct ResponseMessage {
     pub status: u16,
     pub time_in_ms: u128,
@@ -11,15 +9,12 @@ pub struct ResponseMessage {
 
 impl Display for ResponseMessage {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        
-        writeln!(f, "STATUS CODE: {}", self.status)
-        .and_then(|_| writeln!(f, "took: {}ms\n ", self.time_in_ms))
-        .and_then( |_| {
-            for h in self.headers.iter() {
-                writeln!(f, "{h}").unwrap();
-            }
-            Ok(())
-        })
-        .and_then(|_| writeln!(f, "\n{}", self.body))
+        writeln!(f, "random")?;
+        writeln!(f, "STATUS CODE: {}", self.status)?;
+        writeln!(f, "took: {}ms\n ", self.time_in_ms)?;
+        for h in self.headers.iter() {
+            writeln!(f, "{h}")?;
+        }
+        writeln!(f, "\n{}", self.body)
     }
 }
